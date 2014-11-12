bc26b639b591138172edffdf62024234f653db45
htchoi1995 <htchoi1995@gmail.com>
2014-11-12 14:07:51 -0500

    Disable/Enable screenshot sound [1/2]
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3238,6 +3238,12 @@ public final class Settings {
         public static final String QS_COLOR_SWITCH = "qs_color_switch";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
