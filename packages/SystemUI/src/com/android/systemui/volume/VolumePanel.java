f8223066f1f1db680bd22a62e7d470e495f9cfe7
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-03 17:39:41 -0300

    Framework: always update volumes slider state
    Fix a bug when volume slider is not updated when toogling ringer modes or changing Zen mode with expanded volume panel from Media playback
    
    Change-Id: I3f6c01320a53b6ea37abae9145d7d520d3ea46e0
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -619,6 +619,7 @@ public class VolumePanel extends Handler implements DemoMode {
                 mZenPanelExpanded = expanded;
                 updateTimeoutDelay();
                 resetTimeout();
+                updateStates();
             }
         });
     }
